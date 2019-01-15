92c2bed64b38c7c620e248ca1d96aac963af95a9
Shayne Fletcher <shayne.fletcher@digitalasset.com>
2019-01-15 10:14:43 -0500

    prepare-layout : gen-lib-cabal source and cabal file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ghc-lib
+
+`ghc-lib` enables use of the GHC compiler as a library. This Digital Asset (https://www.digitalasset.com) project is a work in progress and not quite ready for public consumption. Check back soon!
---
+++ ghc-lib-gen/src/Main.hs
@@ -0,0 +1,365 @@
+-- -*- truncate-lines: t; -*-
+
+{- Prep. for building GHC as a library. -}
+
+import System.Environment
+import System.Process
+import System.FilePath
+import System.IO.Extra
+import Control.Monad
+import Data.Maybe
+import Data.List.Extra
+import Data.Char
+
+-- Constants.
+
+-- |'cabal' is where the cabal file will be written.
+cabal = "ghc-lib.cabal"
+
+-- |'cabals' is the set of input cabal files consulted.
+cabals :: [FilePath]
+cabals =
+  [
+    "libraries/template-haskell/template-haskell.cabal"
+   , "libraries/ghc-heap/ghc-heap.cabal"
+   , "libraries/ghc-boot-th/ghc-boot-th.cabal"
+   , "libraries/ghc-boot/ghc-boot.cabal"
+   , "libraries/ghci/ghci.cabal"
+   , "compiler/ghc.cabal"
+  ]
+
+-- |'binCabal' is the path to the GHC as an executable cabal file.
+binCabal :: FilePath
+binCabal = "ghc/ghc-bin.cabal"
+
+-- |'extraSrcFiles' is a list of additional source files to be
+-- included in source distributions. The files in this list are all
+-- copied into position by hadrian.
+extraSrcFiles :: [FilePath]
+extraSrcFiles =
+  [
+    "ghc-lib/generated/ghcautoconf.h"
+  , "ghc-lib/generated/ghcplatform.h"
+  , "ghc-lib/generated/ghcversion.h"
+  , "ghc-lib/generated/DerivedConstants.h"
+  , "ghc-lib/generated/GHCConstantsHaskellExports.hs"
+  , "ghc-lib/generated/GHCConstantsHaskellType.hs"
+  , "ghc-lib/generated/GHCConstantsHaskellWrappers.hs"
+  , "ghc-lib/stage1/compiler/build/ghc_boot_platform.h"
+  , "ghc-lib/stage1/compiler/build/primop-can-fail.hs-incl"
+  , "ghc-lib/stage1/compiler/build/primop-code-size.hs-incl"
+  , "ghc-lib/stage1/compiler/build/primop-commutable.hs-incl"
+  , "ghc-lib/stage1/compiler/build/primop-data-decl.hs-incl"
+  , "ghc-lib/stage1/compiler/build/primop-fixity.hs-incl"
+  , "ghc-lib/stage1/compiler/build/primop-has-side-effects.hs-incl"
+  , "ghc-lib/stage1/compiler/build/primop-list.hs-incl"
+  , "ghc-lib/stage1/compiler/build/primop-out-of-line.hs-incl"
+  , "ghc-lib/stage1/compiler/build/primop-primop-info.hs-incl"
+  , "ghc-lib/stage1/compiler/build/primop-strictness.hs-incl"
+  , "ghc-lib/stage1/compiler/build/primop-tag.hs-incl"
+  , "ghc-lib/stage1/compiler/build/primop-vector-tycons.hs-incl"
+  , "ghc-lib/stage1/compiler/build/primop-vector-tys-exports.hs-incl"
+  , "ghc-lib/stage1/compiler/build/primop-vector-tys.hs-incl"
+  , "ghc-lib/stage1/compiler/build/primop-vector-uniques.hs-incl"
+  , "ghc-lib/stage1/compiler/build/Config.hs"
+  -- Technically these are data files (c.f. 'dataFiles' below).
+  , "ghc-lib/stage1/lib/settings"
+  , "ghc-lib/stage1/lib/llvm-targets"
+  , "ghc-lib/stage1/lib/llvm-passes"
+  , "ghc-lib/stage1/lib/platformConstants"
+  ]
+
+-- |'dataDir' is the directory cabal looks for data files to install,
+-- relative to the source directory.
+dataDir :: FilePath
+dataDir = "ghc-lib/stage1/lib"
+
+-- |'dataFiles' is a list of files to be installed for run-time use by
+-- the package.
+dataFiles =
+  [
+    "settings"
+  , "llvm-targets"
+  , "llvm-passes"
+  , "platformConstants"
+  ]
+
+-- Functions for generating files.
+
+-- |'harvest' finds all entries for a given section in the text of a
+-- cabal file.
+harvest :: String -> [String] -> String -> [String]
+harvest tag acc s =
+  let reallyHarvest s =
+        let ls = lines s in
+          let ms =
+                map
+                (dropWhile isSpace)
+                (takeWhile (\s -> not (':' `elem` s)) ls) in
+            harvest tag (acc ++ ms) s in
+    case stripInfix tag s of
+      Nothing -> acc
+      Just (_, '\n' : after) -> reallyHarvest after
+      Just (_, after) -> reallyHarvest after
+
+-- |'withCabals' folds a function over the set of cabal files.
+withCabals :: (String -> String -> [String]) -> String -> IO [String]
+withCabals f root =
+  foldM fun [] (map (root </>) cabals) where
+  fun acc c = do
+      s <- readFile' c
+      return ((f c s) ++ acc)
+
+-- |'modules' extracts a list of modules (e.g. "exposed-modules") from
+-- the set of cabal files.
+modules :: String -> String -> String -> IO [String]
+modules prim alt root =
+  withCabals (\_ s -> nubOrd (harvest prim (harvest alt [] s) s)) root
+
+-- |'otherExtensions' extracts a list of "other-extensions" from the
+-- set of cabal files (playing a bit fast and loose here to be honest).
+otherExtensions :: String -> IO [String]
+otherExtensions root =
+  let f file s =
+        filter
+          (\l -> not (null l || "--" `isPrefixOf` l
+                             || "if flag" `isPrefixOf` l ))
+            (harvest "other-extensions:"
+            (harvest "Other-Extensions:" [] s) s)
+  in do
+       withCabals' f root  where
+         withCabals' f root =
+             foldM fun [] (map (root </>)
+               -- This particular file has ',' and its extensions are
+               -- covered elsewhere.
+               (delete "libraries/ghc-boot/ghc-boot.cabal" cabals)) where
+           fun acc c = do
+                 s <- readFile' c
+                 return (nubOrd ((f c s) ++ acc))
+
+-- |'cSrcs' extracts a list of C source files (i.e. "c-sources") from
+-- the set of cabal files.
+cSrcs :: String -> IO [String]
+cSrcs root =
+  let f file s =
+        let dir = takeDirectory file
+              in map (\f ->
+                         fromMaybe "" (stripPrefix (root ++ "/") (dir </> f))
+                     ) fs
+        where fs = filter
+                (\l -> not (null l || "--" `isPrefixOf` l))
+                (nubOrd (harvest "c-sources:"
+                             (harvest "C-Sources:" [] s) s))
+  in do
+       files <- withCabals f root
+       return $ files
+
+-- |'cmmSrcs' extracts a list of C-- source files (i.e. "cmm-sources")
+-- from the set of cabal files.
+cmmSrcs :: String -> IO [String]
+cmmSrcs root =
+  let f file s =
+        let dir = takeDirectory file
+              in map (\f ->
+                         fromMaybe "" (stripPrefix (root ++ "/") (dir </> f))
+                 ) fs
+        where fs = filter
+                (\l -> not (null l || "--" `isPrefixOf` l))
+                (nubOrd (harvest "cmm-sources:"
+                             (harvest "Cmm-Sources:" [] s) s))
+  in do
+       files <- withCabals f root
+       return $ files
+
+-- |'hsSourceDirs' extracts a list of source directories from the set
+-- of cabal files.
+hsSourceDirs :: String -> IO [String]
+hsSourceDirs root =
+  let f file s =
+        let dir = takeDirectory file
+            dir' = fromMaybe "" (stripPrefix (root ++ "/") dir)
+              in dir' : map (\f ->
+                            fromMaybe "" (stripPrefix (root ++ "/") (dir </> f))
+                           ) fs
+        where fs = filter
+                (\l -> not (null l || "--" `isPrefixOf` l))
+                (nubOrd (harvest "hs-source-dirs:"
+                             (harvest "Hs-Source-Dirs:" [] s) s))
+  in do
+       files <- withCabals f root
+       return $ ("ghc-lib/stage1/compiler/build") : files
+
+-- |'exeOtherExtensions' extracts a list of "other-extensions" from the GHC
+-- as an exe cabal file.
+exeOtherExtensions root = do
+  s <- readFile' $ root </> binCabal
+  return $ filter
+             (\l -> not (null l || "--" `isPrefixOf` l))
+             (nubOrd (harvest "other-extensions:"
+             (harvest "Other-Extensions:" [] s) s))
+
+-- |'exeOtherModules' extracts a list of "other-modules" from the GHC
+-- as an exe cabal file.
+exeOtherModules root = do
+  s <- readFile' $ root </> binCabal
+  return $ filter
+             (\l -> not (null l || "--" `isPrefixOf` l))
+             (nubOrd (harvest "other-modules:"
+             (harvest "Other-Modules:" [] s) s))
+
+-- |'genCabal' produces a cabal file for ghc with supporting
+-- libraries.
+genCabal root = do
+  src <- hsSourceDirs root
+  ems <- modules "exposed-modules:" "Exposed-Modules:" root
+  oms <- modules "other-modules:" "Other-Modules:" root
+  csf <- cSrcs root
+  cmm <- cmmSrcs root
+  oxt <- otherExtensions root
+  eoe <- exeOtherExtensions root
+  eom <- exeOtherModules root
+  let src' = map (\x -> "      " ++ x ++ "\n") src
+      ems' = map (\x -> "      " ++ x ++ "\n") ems
+      oms' = map (\x -> "      " ++ x ++ "\n") oms
+      csf' = map (\x -> "      " ++ x ++ "\n") csf
+      cmm' = map (\x -> "      " ++ x ++ "\n") cmm
+      oxt' = map (\x -> "      " ++ x ++ "\n") oxt
+      eoe' = map (\x -> "      " ++ x ++ "\n") eoe
+      eom' = map (\x -> "      " ++ x ++ "\n") eom
+      dat' = map (\x -> "  " ++ x ++ "\n") dataFiles
+      ext' = map (\x -> "  " ++ x ++ "\n")
+                -- Tiny bit of monkey business here where we separate
+                -- data files genuine source files and add
+                -- libHeapPrim.a as a source file.
+                ("ghc-lib/stage0/libraries/ghc-heap/build/cmm/cbits/libHeapPrim.a" :
+                (let fs = map (\f -> dataDir </> f) dataFiles in
+                            foldl (\acc f -> delete f acc) extraSrcFiles fs))
+  let contents =
+       (unlines [
+
+          -- header
+
+          "cabal-version: 2.1" -- or cabal check complains about cmm-sources
+        , "build-type: Simple"
+        , "name: ghc-lib"
+        , "version: 1.1.0"
+        , "license: BSD-3-Clause"
+        , "license-file: LICENSE"
+        , "category: Development"
+        , "author: XXX"
+        , "maintainer: XXX"
+        , "copyright: XXX"
+        , "synopsis: GHC as a library"
+        , "description: GHC as a library that can be loaded into GHCi."
+        , "homepage: XXX"
+        , "bug-reports: XXX"
+        , "data-dir: " ++ dataDir
+        , "data-files:\n" ++ (concat dat')
+        , "extra-source-files:\n" ++ (concat ext')
+                                  ++ "  includes/*.h\n"
+                                  ++ "  includes/CodeGen.Platform.hs\n"
+                                  ++ "  includes/rts/*.h\n"
+                                  ++ "  includes/rts/storage/*.h\n"
+                                  ++ "  includes/rts/prof/*.h\n"
+                                  ++ "  compiler/nativeGen/*.h\n"
+                                  ++ "  compiler/utils/*.h\n"
+                                  ++ "  compiler/*.h\n"
+        , "tested-with:GHC==8.4.3"
+        , "source-repository head"
+        , "    type: git"
+        , "    location: https://github.com/shayne-fletcher-da/ghc-in-ghci.git"
+        , "Flag with-heap-prim"
+        , "    Description: Link with HeapPrim.a."
+        , "    Default: False"
+
+        -- library
+
+        , "library"
+        , "    default-language:   Haskell2010"
+        , "    default-extensions: NoImplicitPrelude\n"
+        , "    include-dirs:"
+        , "      ghc-lib/generated"
+        , "      ghc-lib/stage1/compiler/build"
+        , "      compiler"
+        , "      compiler/utils"
+        , "    ghc-options: -fobject-code -package=ghc-boot-th -optc-DTHREADED_RTS"
+        , "    cc-options: -DTHREADED_RTS"
+        , "    cpp-options: -DSTAGE=2 -DTHREADED_RTS -DGHCI -DGHC_IN_GHCI"
+        , "    build-depends:"
+        , "      ghc-prim"
+        , "      , base == 4.*, containers, bytestring, binary"
+        , "      , unix, filepath, directory, array, deepseq"
+        , "      , pretty, time, transformers, process, haskeline, hpc"
+        , "    if flag(with-heap-prim)"
+        , "        extra-lib-dirs: ghc-lib/stage0/libraries/ghc-heap/build/cmm/cbits"
+        , "        extra-libraries: HeapPrim"
+        ])
+       ++ "    other-extensions:\n"   ++ (concat oxt')
+       ++ "    c-sources:\n"          ++ (concat csf')
+       ++ "    cmm-sources:\n"        ++ (concat cmm')
+       ++ "    hs-source-dirs:\n"     ++ (concat src')
+       ++ "    exposed-modules:\n"    ++ (concat ems')
+       ++ "    other-modules:\n"      ++ (concat oms')
+       ++ "\n"
+
+       -- exe
+
+       ++ (unlines [
+          "executable ghc-lib"
+        , "    default-language:   Haskell2010"
+        , "    build-depends:"
+        , "        base == 4.*, array, bytestring, directory, process, filepath,"
+        , "        containers, deepseq, ghc-prim, haskeline, time, transformers,"
+        , "        unix, ghc-lib"
+        , "    hs-source-dirs: ghc"
+        , "    ghc-options: -fobject-code -package=ghc-boot-th -optc-DTHREADED_RTS"
+        , "    cc-options: -DTHREADED_RTS"
+        , "    cpp-options: -DGHCI -DTHREADED_RTS -DGHC_LOADED_INTO_GHCI"
+        ])
+       ++ "    other-modules:\n" ++ (concat eom')
+       ++ "    other-extensions:\n" ++ (concat eoe')
+       ++ (unlines [
+          "    default-extensions: NoImplicitPrelude"
+        , "    main-is: Main.hs"
+        ])
+  writeFile (root </> cabal) contents
+
+-- |'genStackYaml' produces a file @stack.Yaml@ in the @root@ directory.
+genStackYaml :: String -> IO ()
+genStackYaml root = do
+  writeFile (root </> "stack.yaml") $
+      unlines ["resolver: lts-12.6", "packages:", "- ."]
+
+genPrerequisites :: String -> IO ()
+genPrerequisites root = do
+  let contents =
+       "(cd " ++ root </> "hadrian && " ++
+       (unlines [
+          "stack build --no-library-profiling && \\"
+        , "stack exec hadrian -- \\"
+        , (" --directory " ++ "\"" ++ root ++ "\" \\")
+        ,  " --configure \\"
+        ,  " --integer-simple\\"
+        ,  " --build-root=\"ghc-lib\" \\"
+        ])
+       -- 'extraSrcFiles' intentionally doesn't contain
+       -- 'libHeapPrim.a'. It is produced in the follow up step below
+       -- (see also 'main' where it's added in to the "extra-sources"
+       -- as an addendum).
+       ++ unlines (map (\l -> l ++ " \\") extraSrcFiles)
+       ++ unlines (["ghc-lib/stage0/libraries/ghc-heap/build/cmm/cbits/HeapPrim.o \\"
+        , ") && (cd " ++ (root </> "ghc-lib" </> "stage0" </> "libraries" </> "ghc-heap" </> "build" </> "cmm" </> "cbits") ++ " && ar -cr libHeapPrim.a HeapPrim.o)"
+        ])
+  putStrLn contents
+  callCommand contents
+
+-- Driver.
+
+-- | 'main' expects a single argument, the root of a GHC source tree.
+main :: IO ()
+main = do
+  [root] <- getArgs
+  genStackYaml root
+  genPrerequisites root
+  genCabal root
+  putStrLn "[Gen_ghc_lib_cabal] Done!"
