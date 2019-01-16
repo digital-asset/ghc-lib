6415a30418d1ec06dcffd670a54c0a4552c8b5eb
Neil Mitchell <ndmitchell@gmail.com>
2019-01-16 10:39:01 +0000

    Initial attempt at a CI experience

---
+++ CI.hs
@@ -0,0 +1,7 @@
+
+
+
+main = do
+    system_ "git clone --recursive --depth=1 https://gitlab.haskell.org/ghc/ghc"
+    withCurrentDirectory "ghc-lib-gen" $ system_ "cabal run ../ghc"
+    withCurrentDirectory "ghc" $ system_ "cabal build lib:ghc-lib"
