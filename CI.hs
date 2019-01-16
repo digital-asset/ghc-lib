0d1bb6cb446f30323fb3c1f7c96a82d5d15fb60e
Neil Mitchell <ndmitchell@gmail.com>
2019-01-16 12:47:06 +0000

    Playing around with the CI script to make it faster

--- CI.hs
+++ CI.hs
@@ -5,10 +5,6 @@ import System.Process.Extra
 
 main :: IO ()
 main = do
-    system_ "curl https://gitlab.haskell.org/ghc/ghc/-/archive/master/ghc-master.tar --output ghc-master.tar"
-    system_ "tar -xf ghc-master.tar"
-
-    system_ "git clone git://git.haskell.org/ghc.git --recursive"
-    system_ "curl -sSL https://get.haskellstack.org/ | sh"
+    system_ "git clone git://git.haskell.org/ghc.git --recurse-submodules=libraries/Cabal"
     withCurrentDirectory "ghc-lib-gen" $ system_ "cabal run ../ghc"
-    withCurrentDirectory "ghc" $ system_ "cabal build lib:ghc-lib"
+    withCurrentDirectory "ghc" $ system_ "cabal build lib:ghc-lib --ghc-option=-O0"
