40f93a2cae85e79d52e458a8b8453f16e63d978e
Neil Mitchell <ndmitchell@gmail.com>
2019-01-16 10:59:12 +0000

    Try cloning from a different location

--- CI.hs
+++ CI.hs
@@ -5,6 +5,6 @@ import System.Process.Extra
 
 main :: IO ()
 main = do
-    system_ "git clone --recursive --depth=1 https://gitlab.haskell.org/ghc/ghc"
+    system_ "git clone --recursive --depth=1 https://gitlab.haskell.org/ghc/ghc.git"
     withCurrentDirectory "ghc-lib-gen" $ system_ "cabal run ../ghc"
     withCurrentDirectory "ghc" $ system_ "cabal build lib:ghc-lib"
