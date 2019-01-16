b6f0a969183a3d5679dd1256537804745f058f79
Shayne Fletcher <shayne.fletcher@digitalasset.com>
2019-01-16 13:38:41 -0500

    mini-hlint : improve error messages

--- examples/mini-hlint/src/Main.hs
+++ examples/mini-hlint/src/Main.hs
@@ -10,6 +10,7 @@ import "ghc-lib" StringBuffer
 import "ghc-lib" Fingerprint
 import "ghc-lib" Lexer
 import "ghc-lib" RdrName
+import "ghc-lib" ErrUtils
 import qualified "ghc-lib" Parser
 
 import FastString
@@ -55,31 +56,40 @@ isNegated (HsApp _ ((L _ (HsVar _ (L _ id)))) _) = id == idNot
 isNegated (HsPar _ (L _ e)) = isNegated e
 isNegated _ = True
 
-analyzeExpr :: Located (HsExpr GhcPs) -> IO ()
-analyzeExpr (L loc expr) = do
+analyzeExpr :: DynFlags -> Located (HsExpr GhcPs) -> IO ()
+analyzeExpr flags (L loc expr) = do
   case expr of
-    HsPar _ x -> analyzeExpr x
-    HsIf _ _ c t f -> do { analyzeExpr c; analyzeExpr t ; analyzeExpr f }
+    HsPar _ x -> analyzeExpr flags x
+    HsIf _ _ c t f -> do {
+        analyzeExpr flags c
+      ; analyzeExpr flags t
+      ; analyzeExpr flags f }
     HsApp _ ((L _ (HsVar _ (L _ id)))) (x@(L _ e)) -> do
         if (id == idNot && isNegated e) then
-          putStrLn (showSDocUnsafe (ppr loc) ++ ": double negation")
+          putStrLn (showSDoc flags (ppr loc)
+                    ++ " : lint : double negation "
+                    ++ "`" ++ showSDoc flags (ppr expr) ++ "'")
         else
-          analyzeExpr x
-    OpApp _ x y z -> do {analyzeExpr x; analyzeExpr y; analyzeExpr z }
+          analyzeExpr flags x
+    OpApp _ x y z -> do {
+          analyzeExpr flags x
+        ; analyzeExpr flags y
+        ; analyzeExpr flags z}
     _ -> return ()
 
-analyzeGHRs (L _ (GRHS _ _ expr)) = analyzeExpr expr
-analyzeGHRs _ = return ()
+analyzeGHRs :: DynFlags -> Located (GRHS GhcPs (LHsExpr GhcPs)) -> IO ()
+analyzeGHRs flags (L _ (GRHS _ _ expr)) = analyzeExpr flags expr
+analyzeGHRs _ _ = return ()
 
-analyzeMatch :: Located (Match GhcPs (LHsExpr GhcPs)) -> IO ()
-analyzeMatch (L _ (Match {m_grhss=(GRHSs {grhssGRHSs=grhss})})) = do
-  forM_ grhss analyzeGHRs
-analyzeMatch _ = return ()
+analyzeMatch :: DynFlags -> Located (Match GhcPs (LHsExpr GhcPs)) -> IO ()
+analyzeMatch flags (L _ (Match {m_grhss=(GRHSs {grhssGRHSs=grhss})})) = do
+  forM_ grhss (analyzeGHRs flags)
+analyzeMatch _ _ = return ()
 
-analyzeDecl :: Located (HsDecl GhcPs) -> IO ()
-analyzeDecl (L _ (ValD _ (FunBind {fun_matches=(MG {mg_alts=(L _ matches)})} ))) = do
-  forM_ matches analyzeMatch
-analyzeDecl _ = return ()
+analyzeDecl :: DynFlags -> Located (HsDecl GhcPs) -> IO ()
+analyzeDecl flags (L _ (ValD _ (FunBind {fun_matches=(MG {mg_alts=(L _ matches)})} ))) = do
+  forM_ matches (analyzeMatch flags)
+analyzeDecl _ _ = return ()
 
 main :: IO ()
 main = do
@@ -87,5 +97,6 @@ main = do
   s <- readFile' filename
   let flags = defaultDynFlags fakeSettings fakeLlvmConfig
   case parse filename flags s of
-    POk _ (L _ (HsModule {hsmodDecls=decls})) -> forM_ decls analyzeDecl
-    PFailed _ _ _ -> putStrLn "Parse failure"
+    POk _ (L _ (HsModule {hsmodDecls=decls})) -> forM_ decls (analyzeDecl flags)
+    PFailed _ loc err ->
+      putStrLn (showSDoc flags (pprLocErrMsg (mkPlainErrMsg flags loc err)))
