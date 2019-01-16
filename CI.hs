c90b08774da5cdcc5a1b9bdd20898db858f3c36e
Neil Mitchell <ndmitchell@gmail.com>
2019-01-16 10:42:43 +0000

    Fix up CI.hs

--- CI.hs
+++ CI.hs
@@ -1,6 +1,9 @@
 
+import System.Directory
+import System.Process.Extra
 
 
+main :: IO ()
 main = do
     system_ "git clone --recursive --depth=1 https://gitlab.haskell.org/ghc/ghc"
     withCurrentDirectory "ghc-lib-gen" $ system_ "cabal run ../ghc"
