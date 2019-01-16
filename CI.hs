6e5c54f58c323ee23dfe3c3dbddcc0f3dc66e718
Neil Mitchell <ndmitchell@gmail.com>
2019-01-16 13:27:16 +0000

    Switch back to cloning everything, for now

--- CI.hs
+++ CI.hs
@@ -5,6 +5,6 @@ import System.Process.Extra
 
 main :: IO ()
 main = do
-    system_ "git clone git://git.haskell.org/ghc.git --recurse-submodules=libraries/Cabal"
+    system_ "git clone git://git.haskell.org/ghc.git --recursive" -- --recurse-submodules=libraries/Cabal"
     withCurrentDirectory "ghc-lib-gen" $ system_ "cabal run ../ghc"
     withCurrentDirectory "ghc" $ system_ "cabal build lib:ghc-lib --ghc-option=-O0"
