565c941b1f4fee402e3b9074b505323821de3ded
Neil Mitchell <ndmitchell@gmail.com>
2019-01-21 10:42:52 +0000

    Abstract out system_ so it prints what is running

--- CI.hs
+++ CI.hs
@@ -2,11 +2,13 @@ import System.Process.Extra
 
 main :: IO ()
 main = do
-    system_ "git clone git://git.haskell.org/ghc.git --recursive" -- --recurse-submodules=libraries/Cabal"
-    system_ "stack exec -- ghc-lib-gen ghc"
+    let cmd x = do putStrLn $ "\n\n# " ++ x; system_ x
+
+    cmd "git clone git://git.haskell.org/ghc.git --recursive" -- --recurse-submodules=libraries/Cabal"
+    cmd "stack exec -- ghc-lib-gen ghc"
 
     -- stack init doesn't work because of https://github.com/commercialhaskell/stack/issues/4508
     appendFile "stack.yaml" $ unlines ["- ghc","- examples/mini-hlint"]
-    system_ "stack build --no-terminal --interleaved-output"
-    system_ "stack exec --no-terminal -- ghc-lib --version"
-    system_ "stack exec --no-terminal -- mini-hlint examples/mini-hlint/test/MiniHlintTest.hs"
+    cmd "stack build --no-terminal --interleaved-output"
+    cmd "stack exec --no-terminal -- ghc-lib --version"
+    cmd "stack exec --no-terminal -- mini-hlint examples/mini-hlint/test/MiniHlintTest.hs"
