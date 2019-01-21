7e4b06fb6fcdced6a300f41d6f0c817fd259e22a
Neil Mitchell <ndmitchell@gmail.com>
2019-01-21 13:09:48 +0000

    Unify and speed up the CI script (#15)
    * Switch Travis to Stack
    
    * Rewrite the CI.hs script to be more like Travis
    
    * Avoid the before_test stanza
    
    * Switch to CI.hs for the script
    
    * Make sure you change the right file name
    
    * Abstract out system_ so it prints what is running
    
    * Instead of doing stack init to produce exactly what we want, just save a stack.yml
    
    * Try and speed up compilation on CI
    
    * Record timings of the steps
    
    * Delete travis.sh, no longer required
    
    * Move the pacman install into the CI script, so we can time it
    
    * Make sure we flush stdout after printing the message
    
    * Explain what the script is
    
    * Try and speed up the Hadrian compilation
    
    * Remove a stray " symbol
    
    * See if OSX works

--- CI.hs
+++ CI.hs
@@ -1,16 +1,30 @@
-import System.Directory
+
+-- CI script, invoked by both Travis and Appveyor
+
+import Control.Monad
+import System.IO
+import System.Info.Extra
 import System.Process.Extra
+import System.Time.Extra
 
 main :: IO ()
 main = do
-    system_ "git clone git://git.haskell.org/ghc.git --recursive" -- --recurse-submodules=libraries/Cabal"
-    system_ "curl -sSL https://get.haskellstack.org/ | sh" -- we'd like to use Cabal
-    withCurrentDirectory "ghc-lib-gen" $ system_ "cabal run ../ghc"
-    withCurrentDirectory "ghc" $ do
-        system_ "cabal configure"
-        system_ "cabal build"
-        system_ "cabal install"
-    withCurrentDirectory "ghc" $
-        system_ "cabal run -- --version"
-    withCurrentDirectory "examples/mini-hlint" $
-        system_ "cabal run test/MiniHlintTest.hs"
+    let cmd x = do
+            putStrLn $ "\n\n# Running: " ++ x
+            hFlush stdout
+            (t, _) <- duration $ system_ x
+            putStrLn $ "# Completed in " ++ showDuration t ++ ": " ++ x ++ "\n"
+            hFlush stdout
+
+    when isWindows $
+        cmd "stack exec -- pacman -S autoconf automake-wrapper make patch python tar --noconfirm"
+    cmd "git clone git://git.haskell.org/ghc.git --recursive" -- --recurse-submodules=libraries/Cabal
+    -- not essential, but make the cache work between Hadrian and ghc-lib and build Hadrian quicker
+    appendFile "ghc/hadrian/stack.yaml" $ unlines ["ghc-options:","  \"$everything\": -O0 -j"]
+    cmd "stack exec -- ghc-lib-gen ghc"
+
+    -- Add the new projects to the stack.yaml, so we compile/test them
+    appendFile "stack.yaml" $ unlines ["- ghc","- examples/mini-hlint"]
+    cmd "stack build --no-terminal --interleaved-output"
+    cmd "stack exec --no-terminal -- ghc-lib --version"
+    cmd "stack exec --no-terminal -- mini-hlint examples/mini-hlint/test/MiniHlintTest.hs"
