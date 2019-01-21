b1a5a6b2fe0541911442b79d0f51f03d9eef1f82
Neil Mitchell <ndmitchell@gmail.com>
2019-01-21 10:22:58 +0000

    Rewrite the CI.hs script to be more like Travis

--- CI.hs
+++ CI.hs
@@ -1,16 +1,12 @@
-import System.Directory
 import System.Process.Extra
 
 main :: IO ()
 main = do
     system_ "git clone git://git.haskell.org/ghc.git --recursive" -- --recurse-submodules=libraries/Cabal"
-    system_ "curl -sSL https://get.haskellstack.org/ | sh" -- we'd like to use Cabal
-    withCurrentDirectory "ghc-lib-gen" $ system_ "cabal run ../ghc"
-    withCurrentDirectory "ghc" $ do
-        system_ "cabal configure"
-        system_ "cabal build"
-        system_ "cabal install"
-    withCurrentDirectory "ghc" $
-        system_ "cabal run -- --version"
-    withCurrentDirectory "examples/mini-hlint" $
-        system_ "cabal run test/MiniHlintTest.hs"
+    system_ "stack exec -- ghc-lib-gen ghc"
+
+    -- stack init doesn't work because of https://github.com/commercialhaskell/stack/issues/4508
+    appendFile "stac.yaml" $ unlines ["- ghc","- examples/mini-hlint"]
+    system_ "stack build --no-terminal --interleaved-output"
+    system_ "stack exec --no-terminal -- ghc-lib --version"
+    system_ "stack exec --no-terminal -- mini-hlint examples/mini-hlint/test/MiniHlintTest.hs"
