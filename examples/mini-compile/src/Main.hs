3d7c6ef9937ec57bd21eb2f312c37886f606bfc7
Shayne Fletcher <shayne.fletcher@digitalasset.com>
2019-01-23 11:39:07 -0500

    mini-compile : rewrite in terms of compileToCoreSimplified

--- examples/mini-compile/src/Main.hs
+++ examples/mini-compile/src/Main.hs
@@ -5,9 +5,6 @@ module Main (main) where
 
 import "ghc-lib" GHC
 import "ghc-lib" Paths_ghc_lib
-import "ghc-lib" BasicTypes
-import "ghc-lib" DriverPhases
-import "ghc-lib" GHC.LanguageExtensions.Type
 import "ghc-lib" HeaderInfo
 import "ghc-lib" Module
 import "ghc-lib" Config
@@ -15,20 +12,53 @@ import "ghc-lib" DynFlags
 import "ghc-lib" Platform
 import "ghc-lib" StringBuffer
 import "ghc-lib" Fingerprint
-import "ghc-lib" ErrUtils
-import "ghc-lib" Lexer
-import qualified "ghc-lib" Parser
-import FastString
+
 import Outputable
-import SrcLoc
 import System.Environment
 import System.IO.Extra
 import qualified Data.Map.Strict as Map
-import Data.Time.Clock
-import Data.Time.Clock.POSIX(posixSecondsToUTCTime)
-import System.FilePath.Posix
-import System.Posix.Types(EpochTime)
-import System.Posix.Files
+import Data.IORef
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [file] -> do
+      s <- readFile' file
+      flags <- mkDynFlags file s
+      dataDir <- getDataDir
+      cm <- runGhc (Just dataDir)
+              (setSessionDynFlags flags
+               >> return file >>= compileToCoreSimplified)
+      putStrLn (showSDocUnsafe (ppr (cm)))
+    _ -> fail "Exactly one file argument required"
+
+mkDynFlags :: String -> String -> IO DynFlags
+mkDynFlags filename s = do {
+  ; dirs_to_clean <- newIORef Map.empty
+  ; files_to_clean <- newIORef emptyFilesToClean
+  ; next_temp_suffix <- newIORef 0
+  ; let baseFlags =
+          (defaultDynFlags fakeSettings fakeLlvmConfig) {
+            ghcLink = NoLink
+          , hscTarget = HscNothing
+          , pkgDatabase = Just []
+          , dirsToClean = dirs_to_clean
+          , filesToClean = files_to_clean
+          , nextTempSuffix = next_temp_suffix
+          , thisInstalledUnitId = toInstalledUnitId (stringToUnitId "ghc-prim")
+          }
+  ; parsePragmasIntoDynFlags filename s baseFlags
+  }
+  where
+    parsePragmasIntoDynFlags :: String -> String -> DynFlags -> IO DynFlags
+    parsePragmasIntoDynFlags fp contents dflags0 = do
+      let opts = getOptions dflags0 (stringToStringBuffer contents) fp
+      (dflags, _, _) <- parseDynamicFilePragma dflags0 opts
+      return dflags
+
+fakeLlvmConfig :: (LlvmTargets, LlvmPasses)
+fakeLlvmConfig = ([], [])
 
 fakeSettings :: Settings
 fakeSettings = Settings
@@ -37,106 +67,26 @@ fakeSettings = Settings
   , sProjectVersion=cProjectVersion
   , sProgramName="ghc"
   , sOpt_P_fingerprint=fingerprint0
+  , sTmpDir="."
   }
   where
     platform =
-      Platform{platformWordSize=8
-              , platformOS=OSUnknown
-              , platformUnregisterised=True}
+      Platform{
+          platformWordSize=8
+        , platformOS=OSUnknown
+        , platformUnregisterised=True
+        }
+    -- This bit may need to be conditional on OS.
     platformConstants =
-      PlatformConstants{pc_DYNAMIC_BY_DEFAULT=False,pc_WORD_SIZE=8}
-
-fakeLlvmConfig :: (LlvmTargets, LlvmPasses)
-fakeLlvmConfig = ([], [])
-
-parse :: String -> DynFlags -> String -> ParseResult (Located (HsModule GhcPs))
-parse filename flags str =
-  unP Parser.parseModule parseState
-  where
-    location = mkRealSrcLoc (mkFastString filename) 1 1
-    buffer = stringToStringBuffer str
-    parseState = mkPState flags buffer location
-
-parsePragmasIntoDynFlags :: String -> String -> DynFlags -> IO DynFlags
-parsePragmasIntoDynFlags fp contents dflags0 = do
-    let opts = getOptions dflags0 (stringToStringBuffer contents) fp
-    (dflags, _, _) <- parseDynamicFilePragma dflags0 opts
-    return dflags
-
-mkModSummary
-  :: String  -- file name
-  -> UTCTime -- file modification time
-  -> DynFlags -- dyn flags
-  -> String  -- file contents
-  -> Located (HsModule GhcPs)
-  -> ModSummary
-mkModSummary filename filetime flags str (L loc rdr_module) = do
-  let mod_name = maybe (GHC.mkModuleName "Main") unLoc $ hsmodName rdr_module
-  let implicit_prelude = xopt ImplicitPrelude flags
-      implicit_imports = mkPrelImports mod_name loc implicit_prelude $ hsmodImports rdr_module
-      mod_loc =
-        ModLocation {
-            ml_hs_file = Just filename
-          , ml_hi_file = replaceExtension filename "hi"
-          , ml_obj_file = replaceExtension filename "o"
-          }
-      imports = [(fmap sl_fs $ ideclPkgQual i, ideclName i)
-                | i <- map GHC.unLoc $ implicit_imports ++ GHC.hsmodImports rdr_module
-                ]
-      InstalledUnitId unit_id = thisInstalledUnitId flags
-    in
-    ModSummary {
-      ms_mod = mkModule (fsToUnitId unit_id) mod_name
-    , ms_location = mod_loc
-    , ms_hs_date = filetime
-    , ms_textual_imps = imports
-    , ms_hspp_file = filename
-    , ms_hspp_opts = flags
-    , ms_hspp_buf = Just (stringToStringBuffer str)
-    -- defaults:
-    , ms_hsc_src = HsSrcFile
-    , ms_obj_date = Nothing
-    , ms_iface_date = Nothing
-    , ms_srcimps = []        -- source imports are not allowed
-    , ms_parsed_mod = Nothing
-    }
-
-epochToUTC :: EpochTime -> UTCTime
-epochToUTC = posixSecondsToUTCTime . realToFrac
-
-setThisInstalledUnitId :: UnitId -> DynFlags -> DynFlags
-setThisInstalledUnitId unitId dflags =
-  dflags {thisInstalledUnitId = toInstalledUnitId unitId}
-
-main :: IO ()
-main = do
-  [filename] <- getArgs
-  s <- readFile' filename
-  flags <- parsePragmasIntoDynFlags
-             filename
-             s
-             (setThisInstalledUnitId
-               (stringToUnitId "ghc-prim")
-               (defaultDynFlags fakeSettings fakeLlvmConfig)
-             )
-  case parse filename flags s of
-    PFailed _ loc err ->
-      putStrLn (showSDoc flags (pprLocErrMsg (mkPlainErrMsg flags loc err)))
-    POk pst rdr_module ->
-      let hpm_annotations =
-            (Map.fromListWith (++) $ annotations pst,
-              Map.fromList ((noSrcSpan,comment_q pst)
-                             :annotations_comments pst))
-      in do
-        status <- getFileStatus filename
-        let mtime = epochToUTC (modificationTime status)
-        let ms = mkModSummary filename mtime flags s rdr_module
-        let pm = ParsedModule {
-                pm_mod_summary = ms
-              , pm_parsed_source = rdr_module
-              , pm_extra_src_files=[] -- src imports not allowed
-              , pm_annotations = hpm_annotations
-              }
-        dataDir <- getDataDir
-        tcm <- runGhc (Just dataDir) (typecheckModule pm >>= \tcm -> return tcm)
-        return ()
+      PlatformConstants {
+        pc_DYNAMIC_BY_DEFAULT=False
+      , pc_WORD_SIZE=8
+      , pc_STD_HDR_SIZE=1
+      , pc_TAG_BITS=3
+      , pc_BLOCKS_PER_MBLOCK=252
+      , pc_BLOCK_SIZE=4096
+      , pc_MIN_PAYLOAD_SIZE=1
+      , pc_MAX_Real_Vanilla_REG=6
+      , pc_MAX_Vanilla_REG=10
+      , pc_MAX_Real_Long_REG=0
+      }
