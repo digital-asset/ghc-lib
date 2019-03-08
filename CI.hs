0a5eb8065e66137fd16e71a63e36bfc979abfdeb
Shayne Fletcher <shayne.fletcher@digitalasset.com>
2019-03-08 09:37:15 -0500

    more travis debugging

--- CI.hs
+++ CI.hs
@@ -18,12 +18,12 @@ main = do
             putStrLn $ "# Completed in " ++ showDuration t ++ ": " ++ x ++ "\n"
             hFlush stdout
     -- debugging
-    cmd "echo 'GET / HTTP/1.0' | openssl s_client -state -nbio -connect gitlab.haskell.org:443"
+    -- cmd "echo 'GET / HTTP/1.0' | openssl s_client -state -nbio -connect gitlab.haskell.org:443"
     when isWindows $
         cmd "stack exec -- pacman -S autoconf automake-wrapper make patch python tar --noconfirm"
     -- We want to use gitlab but Travis has issues:
     -- "fatal: unable to access 'https://gitlab.haskell.org/ghc/ghc.git/': gnutls_handshake() failed: Handshake failed"
-    cmd "git clone https://gitlab.haskell.org/ghc/ghc.git --recursive" -- --recurse-submodules=libraries/Cabal
+    cmd "GIT_CURL_VERBOSE=1 git clone https://gitlab.haskell.org/ghc/ghc.git --recursive" -- --recurse-submodules=libraries/Cabal
     -- not essential, but make the cache work between Hadrian and ghc-lib and build Hadrian quicker
     appendFile "ghc/hadrian/stack.yaml" $ unlines ["ghc-options:","  \"$everything\": -O0 -j"]
     cmd "stack exec -- ghc-lib-gen ghc"
