95f2089b83c1036d2fe9d40d1258ac2ae6897e31
Shayne Fletcher <shayne.fletcher@digitalasset.com>
2019-03-08 22:20:46 -0500

    remove unused langext

--- examples/mini-hlint/src/Main.hs
+++ examples/mini-hlint/src/Main.hs
@@ -2,7 +2,6 @@
 -- SPDX-License-Identifier: (Apache-2.0 OR BSD-3-Clause)
 
 {-# LANGUAGE PackageImports #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -Wno-missing-fields #-}
 
 module Main (main) where
