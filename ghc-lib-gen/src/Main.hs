f03d6ab1cfdc9666a72e197812f6bceb5dfaeea5
Shayne Fletcher <shayne.fletcher@digitalasset.com>
2019-06-16 19:49:25 +0200

    Adjust for ghc-8.8.2-alpha2

--- ghc-lib-gen/src/Main.hs
+++ ghc-lib-gen/src/Main.hs
@@ -243,6 +243,7 @@ parserModules =
   , "PatSyn"
   , "PipelineMonad"
   , "PlaceHolder"
+  , "PlainPanic"
   , "Platform"
   , "PlatformConstants"
   , "Plugins"
@@ -598,16 +599,23 @@ generateGhcLibParserCabal = do
 -- | Run Hadrian to build the things that the Cabal files need.
 generatePrerequisites :: IO ()
 generatePrerequisites = do
+  system_ "./boot"
+  system_ "./configure"
   withCurrentDirectory "hadrian" $ do
-    system_ "stack build --no-library-profiling"
-    system_ $ unwords $
-        ["stack exec hadrian --"
-        ,"--directory=.."
-        ,"--configure"
-        ,"--integer-simple"
-        ,"--build-root=ghc-lib"
-        ] ++ extraFiles ++
-        map (dataDir </>) dataFiles
+    system_ "cabal new-build exe:hadrian"
+  system_ $ unwords $ do
+    ["hadrian/build.sh --integer-simple", "--build-root=$PWD/ghc-lib"]
+    ++ extraFiles ++ map (dataDir </>) dataFiles
+  -- withCurrentDirectory "hadrian" $ do
+  --   system_ "stack build --no-library-profiling"
+  --   system_ $ unwords $
+  --       ["stack exec hadrian --"
+  --       ,"--directory=.."
+  --       ,"--configure"
+  --       ,"--integer-simple"
+  --       ,"--build-root=ghc-lib"
+  --       ] ++ extraFiles ++
+  --       map (dataDir </>) dataFiles
   -- We use the hadrian generated Lexer and Parser so get these out
   -- of the way.
   removeFile "compiler/parser/Lexer.x"
