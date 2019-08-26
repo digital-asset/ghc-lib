cf125e2b008e975c79d63774234207213dbff9c2
Shayne Fletcher <shayne.fletcher@digitalasset.com>
2019-08-26 13:46:17 -0400

    Backport improvements from master (#114)
    * Backport improvements from master
    
    * Don't exec absent tests

--- CI.hs
+++ CI.hs
@@ -1,35 +1,52 @@
--- Copyright (c) 2019, Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
--- SPDX-License-Identifier: (Apache-2.0 OR BSD-3-Clause)
+-- Copyright (c) 2019, Digital Asset (Switzerland) GmbH and/or its
+-- affiliates. All rights reserved.  SPDX-License-Identifier:
+-- (Apache-2.0 OR BSD-3-Clause)
 
--- CI script, invoked by both Travis and Appveyor
+-- CI script, compatible with all of Travis, Appveyor and Azure.
 
-import Control.Monad
+import Control.Monad.Extra
 import System.Directory
 import System.FilePath
 import System.IO.Extra
 import System.Info.Extra
 import System.Process.Extra
 import System.Time.Extra
+import Data.List.Extra
+import Data.Time.Clock
+import Data.Time.Calendar
 
 main :: IO ()
 main = do
+    -- Get packages missing on Windows needed by hadrian.
     when isWindows $
         cmd "stack exec -- pacman -S autoconf automake-wrapper make patch python tar --noconfirm"
 
-    -- Make and extract an sdist of ghc-lib-parser.
+    -- Clear up any detritus left over from previous runs.
+    toDelete <- (["ghc", "ghc-lib", "ghc-lib-parser"] ++) .
+      filter (isExtensionOf ".tar.gz") <$> getDirectoryContents "."
+    forM_ toDelete removePath
+    cmd "git checkout stack.yaml"
+
+    -- Get a clone of ghc.
     cmd "git clone https://gitlab.haskell.org/ghc/ghc.git"
     cmd "cd ghc && git checkout ghc-8.8.1-release"
     cmd "cd ghc && git submodule update --init --recursive"
     appendFile "ghc/hadrian/stack.yaml" $ unlines ["ghc-options:","  \"$everything\": -O0 -j"]
+
     -- Build ghc-lib-gen. Do this here rather than in the Azure script
     -- so that it's not forgotten when testing this program locally.
     cmd "stack --no-terminal build"
+
+    -- Calculate verison and package names.
+    version <- tag
+    let pkg_ghclib = "ghc-lib-" ++ version
+        pkg_ghclib_parser = "ghc-lib-parser-" ++ version
+
+    -- Make and extract an sdist of ghc-lib-parser.
     cmd "stack exec -- ghc-lib-gen ghc --ghc-lib-parser"
-    stackYaml <- readFile' "stack.yaml"
-    writeFile "stack.yaml" $ stackYaml ++ unlines ["- ghc"]
-    tarball <- sDistCreateExtract
-    renameDirectory (dropExtensions tarball) "ghc-lib-parser"
-    removeFile tarball
+    patchVersion version "ghc/ghc-lib-parser.cabal"
+    mkTarball pkg_ghclib_parser
+    renameDirectory pkg_ghclib_parser "ghc-lib-parser"
     removeFile "ghc/ghc-lib-parser.cabal"
     cmd "git checkout stack.yaml"
 
@@ -37,15 +54,15 @@ main = do
     cmd "cd ghc && git checkout ."
     appendFile "ghc/hadrian/stack.yaml" $ unlines ["ghc-options:","  \"$everything\": -O0 -j"]
     cmd "stack exec -- ghc-lib-gen ghc --ghc-lib"
-    stackYaml <- readFile' "stack.yaml"
-    writeFile "stack.yaml" $ stackYaml ++ unlines ["- ghc"]
-    tarball <- sDistCreateExtract
-    renameDirectory (dropExtensions tarball) "ghc-lib"
-    removeFile tarball
+    patchVersion version "ghc/ghc-lib.cabal"
+    patchConstraint version "ghc/ghc-lib.cabal"
+    mkTarball pkg_ghclib
+    renameDirectory pkg_ghclib "ghc-lib"
     removeFile "ghc/ghc-lib.cabal"
     cmd "git checkout stack.yaml"
 
-    -- Test the new projects.
+    -- Append the libraries and examples to stack.yaml.
+    stackYaml <- readFile' "stack.yaml"
     writeFile "stack.yaml" $
       stackYaml ++
       unlines [ "- ghc-lib-parser"
@@ -53,31 +70,57 @@ main = do
               , "- examples/mini-hlint"
               , "- examples/mini-compile"
               ]
-    cmd "stack build --no-terminal --interleaved-output"
-    cmd "stack exec --no-terminal -- ghc-lib --version"
+
+    -- Separate the two library build commands so they are
+    -- independently timed. Note that optimizations in these builds
+    -- are disabled in stack.yaml via `ghc-options: -O0`.
+    cmd "stack build ghc-lib-parser --no-terminal --interleaved-output"
+    cmd "stack build ghc-lib --no-terminal --interleaved-output"
+    cmd "stack build mini-hlint mini-compile --no-terminal --interleaved-output"
+
+    -- Run tests.
     cmd "stack exec --no-terminal -- mini-hlint examples/mini-hlint/test/MiniHlintTest.hs"
     cmd "stack exec --no-terminal -- mini-hlint examples/mini-hlint/test/MiniHlintTest_fatal_error.hs"
     cmd "stack exec --no-terminal -- mini-hlint examples/mini-hlint/test/MiniHlintTest_non_fatal_error.hs"
     cmd "stack exec --no-terminal -- mini-compile examples/mini-compile/test/MiniCompileTest.hs"
-
-    -- Test everything loads in GHCi, see https://github.com/digital-asset/ghc-lib/issues/27
+    -- Test everything loads in GHCi, see
+    -- https://github.com/digital-asset/ghc-lib/issues/27
     cmd "stack exec --no-terminal -- ghc -package=ghc-lib-parser -e \"print 1\""
     cmd "stack exec --no-terminal -- ghc -package=ghc-lib -e \"print 1\""
     where
-      dropExtensions :: String -> String
-      dropExtensions = dropExtension . dropExtension
-
       cmd :: String -> IO ()
       cmd x = do
-            putStrLn $ "\n\n# Running: " ++ x
-            hFlush stdout
-            (t, _) <- duration $ system_ x
-            putStrLn $ "# Completed in " ++ showDuration t ++ ": " ++ x ++ "\n"
-            hFlush stdout
+        putStrLn $ "\n\n# Running: " ++ x
+        hFlush stdout
+        (t, _) <- duration $ system_ x
+        putStrLn $ "# Completed in " ++ showDuration t ++ ": " ++ x ++ "\n"
+        hFlush stdout
 
-      sDistCreateExtract :: IO String
-      sDistCreateExtract = do
+      mkTarball :: String -> IO ()
+      mkTarball target = do
+        writeFile "stack.yaml" . (++ "- ghc\n") =<< readFile' "stack.yaml"
         cmd "stack sdist ghc --tar-dir=."
-        [tarball] <- filter (isExtensionOf ".tar.gz") <$> getDirectoryContents "."
-        cmd $ "tar -xf " ++ tarball
-        return tarball
+        cmd $ "tar -xvf " ++ target ++ ".tar.gz"
+
+      tag :: IO String
+      tag = do
+        UTCTime day _ <- getCurrentTime
+        return $ "8.8.1." ++ replace "-" "" (showGregorian day)
+
+      patchVersion :: String -> FilePath -> IO ()
+      patchVersion version file =
+        writeFile file .
+          replace "version: 0.1.0" ("version: " ++ version)
+          =<< readFile' file
+
+      patchConstraint :: String -> FilePath -> IO ()
+      patchConstraint version file =
+        writeFile file .
+          replace "ghc-lib-parser" ("ghc-lib-parser == " ++ version)
+          =<< readFile' file
+
+      removePath :: FilePath -> IO ()
+      removePath p =
+        whenM (doesPathExist p) $ do
+          putStrLn ("# Removing " ++ p)
+          removePathForcibly p
---
+++ ghc-lib-gen/src/GhclibgenOpts.hs
@@ -0,0 +1,59 @@
+-- Copyright (c) 2019, Digital Asset (Switzerland) GmbH and/or its
+-- affiliates. All rights reserved.  SPDX-License-Identifier:
+-- (Apache-2.0 OR BSD-3-Clause)
+
+module GhclibgenOpts(
+    GhclibgenTarget(..)
+  , GhclibgenOpts(..)
+  , ghclibgenVersion
+  , ghclibgenOpts
+) where
+
+import Control.Applicative
+import Options.Applicative
+import Data.Maybe
+import Data.Version (showVersion)
+import Data.Semigroup ((<>))
+import Paths_ghc_lib_gen (version)
+
+-- | A ghc-lib-gen target.
+data GhclibgenTarget = GhclibParser | Ghclib
+
+-- | The type of ghc-lib-gen options.
+data GhclibgenOpts = GhclibgenOpts {
+    ghclibgenOpts_root :: !FilePath -- ^ Path to a GHC git repository.
+  , ghclibgenOpts_target :: !GhclibgenTarget -- ^ What target?
+ }
+
+-- | A parser of the "--ghc-lib" target.
+ghclib :: Parser GhclibgenTarget
+ghclib = flag' Ghclib
+  ( long "ghc-lib"
+  <> help "Generate a ghc-lib.cabal"
+  )
+
+-- | A parser of the "--ghc-lib-parser" target.
+ghclibParser :: Parser GhclibgenTarget
+ghclibParser = flag' GhclibParser
+  ( long "ghc-lib-parser"
+  <> help "Generate a ghc-lib-parser.cabal"
+  )
+
+-- | A parser of "--version".
+ghclibgenVersion :: Parser (a -> a)
+ghclibgenVersion =
+  infoOption
+  (showVersion version)
+  (long "version" <> help "Show version")
+
+-- | A parser of a ghc-lib-gen target: `target := | "--ghc-lib-parser"
+-- | "--ghc-lib" | /* nothing */`.
+ghclibgenTarget :: Parser GhclibgenTarget
+ghclibgenTarget =
+  fromMaybe Ghclib Control.Applicative.<$> optional (ghclibParser <|> ghclib)
+
+-- | A parser of ghc-lib-gen options: `opts := STRING target`.
+ghclibgenOpts :: Parser GhclibgenOpts
+ghclibgenOpts = GhclibgenOpts
+  <$> argument str (metavar "GHC_ROOT")
+  <*> ghclibgenTarget
