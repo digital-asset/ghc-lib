a6d8eb1454768c7349e04564895340dd41304530
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2019-08-27 15:40:02 +0200

    make CI.hs executable

--- CI.hs
+++ CI.hs
@@ -1,3 +1,4 @@
+#!/usr/bin/env stack runhaskell --package extra --package optparse-applicative --
 -- Copyright (c) 2019, Digital Asset (Switzerland) GmbH and/or its
 -- affiliates. All rights reserved.  SPDX-License-Identifier:
 -- (Apache-2.0 OR BSD-3-Clause)
