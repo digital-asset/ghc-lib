55f5871b289c0eedc7d1ed4117d18cd31f98eeb9
Shayne Fletcher <shayne.fletcher@digitalasset.com>
2019-09-16 07:43:44 -0400

    Update sha, remove hadrian hack

--- CI.hs
+++ CI.hs
@@ -44,7 +44,7 @@ data GhcFlavor = Ghc881 | DaGhc881 | GhcMaster String
 
 -- Last tested gitlab.haskell.org/ghc/ghc.git at
 current :: String
-current =    "bd079345f633e01e8d4ff76aef39cb2aa50af6a9" -- 09/14/2019
+current =    "7208160d2caae125479e8dd39a263620ea7e0ffe" -- 09/16/2019
           -- "270fbe8512f04b6107755fa22bdec62205c0a567" -- 09/09/2019
           -- "b55ee979d32df938eee9c4c02c189f8be267e8a1" -- 09/06/2019
           -- "11679e5bec1994775072e8e60f24b4ce104af0a7" -- 09/03/2019
@@ -173,7 +173,6 @@ buildDists ghcFlavor resolver = do
 
     -- Make and extract an sdist of ghc-lib-parser.
     appendFile "ghc/hadrian/stack.yaml" $ unlines ["ghc-options:","  \"$everything\": -O0 -j"]
-    appendFile "ghc/hadrian/stack.yaml" $ unlines ["allow-newer: True"]
     -- Feedback on the compiler used for ghc-lib-gen.
     -- Feedback on the compiler used for ghc-lib-gen.
     stack $ "exec -- ghc-lib-gen ghc --ghc-lib-parser " ++ ghcFlavorOpt ghcFlavor
@@ -186,7 +185,6 @@ buildDists ghcFlavor resolver = do
     -- Make and extract an sdist of ghc-lib.
     cmd "cd ghc && git checkout ."
     appendFile "ghc/hadrian/stack.yaml" $ unlines ["ghc-options:","  \"$everything\": -O0 -j"]
-    appendFile "ghc/hadrian/stack.yaml" $ unlines ["allow-newer: True"]
     -- Feedback on the compiler used for ghc-lib-gen.
     stack $ "exec -- ghc-lib-gen ghc --ghc-lib " ++ ghcFlavorOpt ghcFlavor
     patchVersion version "ghc/ghc-lib.cabal"
